ed8d5b4037b6b37008ab092a1cfecb049fdf19f2
jguyet <jguyet.weoohh@gmail.com>
2024-11-20 02:49:29 +0100

    Publish v0.50.10-helios-21

--- publish.sh
+++ publish.sh
@@ -1,11 +1,11 @@
-VERSION=${VERSION:-"v0.50.10-helios-20"}
+VERSION=${VERSION:-"v0.50.10-helios-21"}
 
-echo "Deploy cometbft"
+echo "Deploy sdk-go"
 git add .
 git commit -m "Publish $VERSION"
 git push
 git tag $VERSION
 git push origin $VERSION
-GOPROXY=proxy.golang.org go list -m github.com/Helios-Chain-Labs/cometbft@$VERSION
+GOPROXY=proxy.golang.org go list -m github.com/Helios-Chain-Labs/sdk-go@$VERSION
 
 echo "Publish done"
