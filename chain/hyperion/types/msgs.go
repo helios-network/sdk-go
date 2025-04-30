8ceb177ccc349b188a48300579eeb5fbb45ba6be
jguyet <jguyet.weoohh@gmail.com>
2025-04-30 02:31:55 +0200

    Update Attestation and claim messages with rpcUsed

--- chain/hyperion/types/msgs.go
+++ chain/hyperion/types/msgs.go
@@ -292,6 +292,8 @@ type EthereumClaim interface {
 	// validators claims agree. Therefore it's extremely important that this include all elements of the claim
 	// with the exception of the orchestrator who sent it in, which will be used as a different part of the index
 	ClaimHash() []byte
+
+	GetRpcUsed() string
 }
 
 var (
